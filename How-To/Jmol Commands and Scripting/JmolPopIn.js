3119ce86cf5bd76b9c27066909bf5c00bfe3c36f
gutow <gutow@chemistrymac08.local>
2014-01-27 08:52:44 -0600

    Commands and Scripting to JSmol

--- How-To/Jmol Commands and Scripting/JmolPopIn.js
+++ How-To/Jmol Commands and Scripting/JmolPopIn.js
@@ -1,7 +1,7 @@
 /*	Allows to open Jmol models on request, in a div
 	
 	Available from  http://biomodel.uah.es/Jmol/  and  http://wiki.jmol.org/ 
-	Author: Angel Herráez.  Version 2007.04.23
+	Author: Angel Herr?ez.  Version 2007.04.23
 	
 	This template is offered freely for anyone to use or adapt it, 
 	according to Creative Commons Attribution-ShareAlike 3.0 License,
@@ -17,28 +17,51 @@
 	Modified 2007.08.13 by Bob Hanson
 	
 	-- integration into Jmol application
+	
+	Modified 2010.3.9 by Jonathan Gutow
+	-- addition of widget activation upon pop-in.
+	-- addition of widget switching on view change in ScriptButton pages.
+	
+	Modified 2010 June by Jonathan Gutow and Angel Herraez
+	-- bug fixes for IE support
 
 */
 
 function addJavaScript(path, file) {
- document.write("<"+"script src=\"" + path + "/" + file + "\" type=\"text/javascript\"><" + "/script>"); 
+    if (path.charAt(path.length-1)!="/") { path += "/"; }
+    document.write("<"+"script src=\"" + path + file + "\" type=\"text/javascript\"><" + "/script>"); 
 }
 
 
 function putJmolDiv(molNr, molFileName,imageFileName,appletWidth,appletHeight) {
-	var tx = '<table cellpadding="10"><tr><td style="color: rgb(255, 255, 51);">';
-	tx += 'To get a 3-D model you can manipulate, click ';
-	tx += '<a href="javascript:void(popInJmol(' + molNr + ', \'' + molFileName + '\','+ appletWidth + ','+ appletHeight + '))">here</a>.';
-	tx += ' Download time may be significant the first time the applet is loaded.</td></tr></table>';
-	document.writeln(tx);
+	var tx = '<table cellpadding="10"><tr><td style="color: rgb(255, 255, 51);">'
+	    + 'To get a 3-D model you can manipulate, click <a href="HREF">here</a>. Download time may be significant the first time the applet is loaded.'
+		+ '</td></tr></table>';
+	document.writeln(tx.replace(/HREF/,'javascript:void(popInJmol(' + molNr + ', \'' + molFileName + '\','+ appletWidth + ','+ appletHeight + '))'));
 }
 
 function popInJmol(n,fileName,width,height) {
 	document.getElementById("Jmol"+n).innerHTML = jmolApplet([width,height],"defaultDirectory = \""+dataDir+"\";script "+fileName+"",n);
+    var cntlID =  "JmolCntl"+n;
+    var cntlDiv = document.getElementById(cntlID)
+    if(cntlDiv)
+        cntlDiv.style.display = 'block';
 }
 
-function scriptbuttonJmol(fileName) {
+function scriptbuttonJmol(fileName, numberOfButtons) {
 	document.getElementById("Jmol0").innerHTML = jmolApplet("100%","defaultDirectory = \""+dataDir+"\";script "+fileName+"");
+}
+
+function fixScriptButtonWidgets(numberOfButtons){
+    for (i=0;i<numberOfButtons;i++) {
+        var divID = "jmolCntl"+i;
+        if(divID) jmolWidgetStrs[i]=new String(document.getElementById(divID).innerHTML);
+    }
+    document.getElementById("JmolCntl").innerHTML = jmolWidgetStrs[0];
+}
+
+function updateScriptButtonWidgets(buttonNumber){
+    document.getElementById("JmolCntl").innerHTML = jmolWidgetStrs[buttonNumber];
 }
 
 function getHTML(element) {
@@ -83,5 +106,6 @@ function changePathIfLocal(){
   if (protocol == "file:") { 
     jmoljarpath = jmollocaljarpath;
     jmoljspath = jmollocaljspath;
+    jmoljarfile = jmollocaljar;
   }
 }
